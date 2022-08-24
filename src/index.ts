24f6c3ff609799c53a68bf65c568cf6c2f163e75
carlos.campinho_mirakulo <carlos.campinho_mirakulo@prestador.globo>
2022-08-24 17:59:00 -0300

    generics

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('TESTE')
+function unionObject<T, U>(object1: T, object2: U): T & U {
+  // return { ...object1, ...object2 };
+  return Object.assign({}, object1, object2)
+}
+
+const object1 = { key1: 'value1' }
+const object2 = { key2: 'value2' }
+const union = unionObject(object1, object2)
+console.log(union)
